1c9878fe369b38d7c51eba66527d9ac1cb0e5aff
HarmonyHuang <harmony.j.huang@gmail.com>
2025-06-01 11:14:44 +0800

    Update hermes_scraper_with_line_push.py

--- hermes_scraper_with_line_push.py
+++ hermes_scraper_with_line_push.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
 import pandas as pd
 import yagmail
 import gspread
